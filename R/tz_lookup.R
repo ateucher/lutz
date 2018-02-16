f02cb160bbbd031d25673a4d327083f4a3f59ab8
Andy Teucher <andy.teucher@gmail.com>
2018-02-15 22:41:18 -0800

    Initial version with map-based lookup

--- R/tz_lookup.R
+++ R/tz_lookup.R
@@ -1,4 +1,10 @@
-#' Lookup time zones of points
+#' Lookup time zones of sf or sp points
+#'
+#' This can very quickly look up the time zone of many thousands of points,
+#' however the underlying time zone map has been simplified to allow for this speed.
+#' As such when a point is near a time zone boundary and not near a populated centre,
+#' it may return the incorrect timezone. If accuracy is more important than speed,
+#' try [tz_lookup2()].
 #'
 #' @param x either an `sfc` or `sf` points or `SpatialPoints(DataFrame)` object
 #' @param crs the coordinate reference system: integer with the EPSG code, or character with proj4string.
@@ -27,21 +33,7 @@ tz_lookup <- function(x, crs = NULL) {
 
 #' @export
 tz_lookup.sf <- function(x, crs = NULL) {
-  if (!requireNamespace("sf"))
-    stop("You must have the sf package installed to use this function", call. = FALSE)
-
-  if (!all(sf::st_geometry_type(x) == "POINT"))
-    stop("This only works with points", call. = FALSE)
-
-  if (is.na(sf::st_crs(x))) {
-    if (is.null(crs)) crs <- 4326
-    sf::st_crs(x) <- crs
-  }
-
-  transform <- sf::st_crs(x) != sf::st_crs(4326)
-  if (transform) {
-    x <- sf::st_transform(x, crs = 4326)
-  }
+  x <- fix_sf(x, crs)
 
   coords <- sf::st_coordinates(x)
   # sf stores as x, y and tzlookup likes lat, lon (Which is the opposite)
@@ -53,26 +45,20 @@ tz_lookup.sfc <- tz_lookup.sf
 
 #' @export
 tz_lookup.SpatialPoints <- function(x, crs = NULL) {
-  if (!requireNamespace("sp"))
-    stop("You must have the sp package installed to use this function", call. = FALSE)
-
-  if (is.numeric(crs)) crs <- paste0("+init=epsg:", crs)
-
-  if (is.na(sp::proj4string(x))) {
-    if (is.null(crs)) crs <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
-    sp::proj4string(x) <- sp::CRS(crs)
-  }
-
-  if (!is_wgs84(x)) {
-    x <- sp::spTransform(x, sp::CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
-  }
+  x <- fix_sp(x, crs)
 
   coords <- sp::coordinates(x)
   # sf stores as x, y and tzlookup likes lat, lon (Which is the opposite)
   tz_lookup_coords(lat = coords[, 2], lon = coords[, 1])
 }
 
-#' Lookup time zones of points
+#' Lookup time zones of lat/long pairs
+#'
+#' This can very quickly look up the time zone of many thousands of points,
+#' however the underlying time zone map has been simplified to allow for this speed.
+#' As such when a point is near a time zone boundary and not near a populated centre,
+#' it may return the incorrect timezone. If accuracy is more important than speed,
+#' try [tz_lookup_coords2()].
 #'
 #' @param lat numeric vector of latitudes
 #' @param lon numeric vector of longitudes the same length as `x`
@@ -108,4 +94,89 @@ is_wgs84 <- function(x) {
   comp <- c("+proj=longlat", "+datum=WGS84", "+no_defs")
   x_str <- strsplit(sp::CRS(sp::proj4string(x))@projargs, " +")[[1]]
   all(comp %in% x_str)
+}
+
+fix_sp <- function(x, crs) {
+  if (!requireNamespace("sp"))
+    stop("You must have the sp package installed to use this function", call. = FALSE)
+
+  if (is.numeric(crs)) crs <- paste0("+init=epsg:", crs)
+
+  if (is.na(sp::proj4string(x))) {
+    if (is.null(crs)) crs <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+    sp::proj4string(x) <- sp::CRS(crs)
+  }
+
+  if (!is_wgs84(x)) {
+    x <- sp::spTransform(x, sp::CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+  }
+  x
+}
+
+fix_sf <- function(x, crs) {
+  if (!requireNamespace("sf"))
+    stop("You must have the sf package installed to use this function", call. = FALSE)
+
+  if (!all(sf::st_geometry_type(x) == "POINT"))
+    stop("This only works with points", call. = FALSE)
+
+  if (is.na(sf::st_crs(x))) {
+    if (is.null(crs)) crs <- 4326
+    sf::st_crs(x) <- crs
+  }
+
+  transform <- sf::st_crs(x) != sf::st_crs(4326)
+  if (transform) {
+    x <- sf::st_transform(x, crs = 4326)
+  }
+  x
+}
+
+#' A more accurate (but slower) version of tz_lookup
+#'
+#' @inheritParams tz_lookup
+#'
+#' @inherit tz_lookup return
+#' @export
+tz_lookup2 <- function(x, crs) {
+  UseMethod("tz_lookup2")
+}
+
+#' @export
+tz_lookup2.sf <- function(x, crs = NULL) {
+  x <- fix_sf(x, crs)
+  x <- suppressMessages(sf::st_join(x, tz_sf))
+  ret <- x$tzid
+
+  # If any are NA (probably ocean), try to fill in with V8-based tzlookup
+  nas <- which(is.na(ret))
+  if (!length(nas)) {
+    return(ret)
+  }
+  ret[nas] <- tz_lookup(x[nas, ])
+  ret
+}
+
+#' @export
+tz_lookup2.sfc <- function(x, crs = NULL) {
+  x_sf <- sf::st_sf(id = seq_len(length(x)), geom = x)
+  tz_lookup2(x_sf, crs = crs)
+}
+
+#' @export
+tz_lookup2.SpatialPoints <- function(x, crs) {
+  x <- sf::st_as_sf(x)
+  tz_lookup2(x, crs)
+}
+
+#' More accurate (but slower) version of `tz_lookup_coords`
+#'
+#' @inheritParams tz_lookup_coords
+#'
+#' @inherit tz_lookup_coords return description
+#' @export
+tz_lookup_coords2 <- function(lat, lon) {
+  ll <- data.frame(lat = lat, lon = lon)
+  ll_sf <- sf::st_as_sf(ll, coords = c("lon", "lat"), crs = 4326)
+  tz_lookup2(ll_sf)
 }
---
+++ data-raw/tz_sf.R
@@ -0,0 +1,9 @@
+library(sf)
+library(rmapshaper)
+download.file("https://github.com/evansiroky/timezone-boundary-builder/releases/download/2017c/timezones.geojson.zip",
+              destfile = "data-raw/tz.zip")
+unzip("data-raw/tz.zip", exdir = "data-raw", overwrite = TRUE)
+tz_full <- read_sf(list.files("data-raw", recursive = TRUE, pattern = "\\json$", full.names = TRUE))
+tz_sf <- ms_simplify(tz_full, keep = 0.1, keep_shapes = TRUE, explode = TRUE)
+
+devtools::use_data(tz_sf, compress = "xz", internal = TRUE, overwrite = TRUE)
