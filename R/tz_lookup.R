de85c56f30bd79e3b54447d2f8aa9fdeda79f8c3
Andy Teucher <andy.teucher@gmail.com>
2018-03-02 17:24:47 -0800

    Put utility functions in utils.R

--- R/tz_lookup.R
+++ R/tz_lookup.R
@@ -84,48 +84,7 @@ tz_lookup_coords <- function(lat, lon) {
   ret
 }
 
-is_wgs84 <- function(x) {
-  # The most minimal specification of WGS84
-  comp <- c("+proj=longlat", "+datum=WGS84", "+no_defs")
-  x_str <- strsplit(sp::CRS(sp::proj4string(x))@projargs, " +")[[1]]
-  all(comp %in% x_str)
-}
 
-fix_sp <- function(x, crs) {
-  if (!requireNamespace("sp"))
-    stop("You must have the sp package installed to use this function", call. = FALSE)
-
-  if (is.numeric(crs)) crs <- paste0("+init=epsg:", crs)
-
-  if (is.na(sp::proj4string(x))) {
-    if (is.null(crs)) crs <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
-    sp::proj4string(x) <- sp::CRS(crs)
-  }
-
-  if (!is_wgs84(x)) {
-    x <- sp::spTransform(x, sp::CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
-  }
-  x
-}
-
-fix_sf <- function(x, crs) {
-  if (!requireNamespace("sf"))
-    stop("You must have the sf package installed to use this function", call. = FALSE)
-
-  if (!all(sf::st_geometry_type(x) == "POINT"))
-    stop("This only works with points", call. = FALSE)
-
-  if (is.na(sf::st_crs(x))) {
-    if (is.null(crs)) crs <- 4326
-    sf::st_crs(x) <- crs
-  }
-
-  transform <- sf::st_crs(x) != sf::st_crs(4326)
-  if (transform) {
-    x <- sf::st_transform(x, crs = 4326)
-  }
-  x
-}
 
 #' A more accurate (but slower) version of tz_lookup
 #'
@@ -181,18 +140,4 @@ tz_lookup_coords2 <- function(lat, lon) {
     ret[cc] <- tz_lookup2(ll_sf)
   }
   ret
-}
-
-check_coords <- function(lat, lon) {
-  if (!identical(length(lat), length(lon)) || !all(is.numeric(lat) && is.numeric(lon))) {
-    stop("lat and lon must numeric vectors be of the same length")
-  }
-}
-
-check_for_spatial <- function(x, suffix = "") {
-  if (inherits(x, c("sf", "sfc", "SpatialPoints"))) {
-    stop(sprintf("It looks like you are trying to get the tz of an sf/sfc or SpatialPoints object! Use tz_lookup%s() instead.",
-                 suffix),
-         call. = FALSE)
-  }
 }
---
+++ R/utils.R
@@ -0,0 +1,56 @@
+is_wgs84 <- function(x) {
+  # The most minimal specification of WGS84
+  comp <- c("+proj=longlat", "+datum=WGS84", "+no_defs")
+  x_str <- strsplit(sp::CRS(sp::proj4string(x))@projargs, " +")[[1]]
+  all(comp %in% x_str)
+}
+
+fix_sp <- function(x, crs) {
+  if (!requireNamespace("sp"))
+    stop("You must have the sp package installed to use this function", call. = FALSE)
+
+  if (is.numeric(crs)) crs <- paste0("+init=epsg:", crs)
+
+  if (is.na(sp::proj4string(x))) {
+    if (is.null(crs)) crs <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+    sp::proj4string(x) <- sp::CRS(crs)
+  }
+
+  if (!is_wgs84(x)) {
+    x <- sp::spTransform(x, sp::CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+  }
+  x
+}
+
+fix_sf <- function(x, crs) {
+  if (!requireNamespace("sf"))
+    stop("You must have the sf package installed to use this function", call. = FALSE)
+
+  if (!all(sf::st_geometry_type(x) == "POINT"))
+    stop("This only works with points", call. = FALSE)
+
+  if (is.na(sf::st_crs(x))) {
+    if (is.null(crs)) crs <- 4326
+    sf::st_crs(x) <- crs
+  }
+
+  transform <- sf::st_crs(x) != sf::st_crs(4326)
+  if (transform) {
+    x <- sf::st_transform(x, crs = 4326)
+  }
+  x
+}
+
+check_coords <- function(lat, lon) {
+  if (!identical(length(lat), length(lon)) || !all(is.numeric(lat) && is.numeric(lon))) {
+    stop("lat and lon must numeric vectors be of the same length")
+  }
+}
+
+check_for_spatial <- function(x, suffix = "") {
+  if (inherits(x, c("sf", "sfc", "SpatialPoints"))) {
+    stop(sprintf("It looks like you are trying to get the tz of an sf/sfc or SpatialPoints object! Use tz_lookup%s() instead.",
+                 suffix),
+         call. = FALSE)
+  }
+}
