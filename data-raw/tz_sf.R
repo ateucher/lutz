27f2b8e32acdfad5d39ef47d36b55862e6ac3bb3
Andy Teucher <andy.teucher@gmail.com>
2018-06-23 09:30:25 -0700

    Use tz data with oceans, simplify teeny bit more

--- data-raw/tz_sf.R
+++ data-raw/tz_sf.R
@@ -2,10 +2,10 @@ library(sf)
 library(rmapshaper)
 # The data is from https://github.com/evansiroky/timezone-boundary-builder
 # And released under the Open Data Commons Open Database License (ODbL)
-download.file("https://github.com/evansiroky/timezone-boundary-builder/releases/download/2018d/timezones.geojson.zip",
+download.file("https://github.com/evansiroky/timezone-boundary-builder/releases/download/2018d/timezones-with-oceans.geojson.zip",
               destfile = "data-raw/tz.zip")
 unzip("data-raw/tz.zip", exdir = "data-raw", overwrite = TRUE)
-tz_full <- read_sf(list.files("data-raw", recursive = TRUE, pattern = "\\json$", full.names = TRUE))
-tz_sf <- ms_simplify(tz_full, keep = 0.2, keep_shapes = TRUE, explode = TRUE)
+tz_full <- read_sf(list.files("data-raw", recursive = TRUE, pattern = "oceans\\.json$", full.names = TRUE))
+tz_sf <- ms_simplify(tz_full, keep = 0.18, keep_shapes = TRUE, explode = TRUE)
 
 devtools::use_data(tz_sf, compress = "xz", internal = TRUE, overwrite = TRUE)
