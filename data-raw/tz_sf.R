d65b511ae19b6e3f9d794122247008ed63271ab1
Andy Teucher <andy.teucher@gmail.com>
2019-07-11 13:51:27 -0700

    Add updated tz boundaries and js object

--- data-raw/tz_sf.R
+++ data-raw/tz_sf.R
@@ -2,10 +2,10 @@ library(sf)
 library(rmapshaper)
 # The data is from https://github.com/evansiroky/timezone-boundary-builder
 # And released under the Open Data Commons Open Database License (ODbL)
-download.file("https://github.com/evansiroky/timezone-boundary-builder/releases/download/2019a/timezones-with-oceans.geojson.zip",
+download.file("https://github.com/evansiroky/timezone-boundary-builder/releases/download/2019b/timezones-with-oceans.geojson.zip",
               destfile = "data-raw/tz.zip")
 unzip("data-raw/tz.zip", exdir = "data-raw", overwrite = TRUE)
 tz_full <- read_sf(list.files("data-raw", recursive = TRUE, pattern = "oceans\\.json$", full.names = TRUE))
-tz_sf <- ms_simplify(tz_full, keep = 0.16, keep_shapes = TRUE, explode = TRUE)
+tz_sf <- ms_simplify(tz_full, keep = 0.139, keep_shapes = TRUE, explode = TRUE)
 
 usethis::use_data(tz_sf, compress = "xz", internal = TRUE, overwrite = TRUE)
