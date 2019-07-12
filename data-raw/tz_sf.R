df37292ccde7ea629ea9152619093378803e7cef
Andy Teucher <andy.teucher@gmail.com>
2019-07-11 21:35:49 -0700

    Simplify tz_sf further, make valid

--- data-raw/tz_sf.R
+++ data-raw/tz_sf.R
@@ -1,4 +1,5 @@
 library(sf)
+library(lwgeom)
 library(rmapshaper)
 # The data is from https://github.com/evansiroky/timezone-boundary-builder
 # And released under the Open Data Commons Open Database License (ODbL)
@@ -6,6 +7,7 @@ download.file("https://github.com/evansiroky/timezone-boundary-builder/releases/
               destfile = "data-raw/tz.zip")
 unzip("data-raw/tz.zip", exdir = "data-raw", overwrite = TRUE)
 tz_full <- read_sf(list.files("data-raw", recursive = TRUE, pattern = "oceans\\.json$", full.names = TRUE))
-tz_sf <- ms_simplify(tz_full, keep = 0.139, keep_shapes = TRUE, explode = TRUE)
+tz_sf <- ms_simplify(tz_full, keep = 0.135, keep_shapes = TRUE, explode = TRUE) %>%
+  st_make_valid()
 
 usethis::use_data(tz_sf, compress = "xz", internal = TRUE, overwrite = TRUE)
